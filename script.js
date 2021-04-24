e00b1093fcd7aadac04396b4b02b82fdd8c82105
Matthias Oppermann <mfoppermann@gmail.com>
2021-04-24 15:13:43 +0200

    Create object with 3 message components

---
+++ script.js
@@ -0,0 +1,5 @@
+const messageComponents = {
+    firstPart: ['Inspire', 'Motivate', 'Excite'],
+    secondPart: ['your friends', 'your colleagues', 'your family'],
+    thirdPart: ['to do great things', 'to be there for others', 'to create something new']
+};
