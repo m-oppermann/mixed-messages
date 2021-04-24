2b9973c6659f20436e21ff454de52ed3e4d80bd2
Matthias Oppermann <mfoppermann@gmail.com>
2021-04-24 16:10:58 +0200

    Select the different message components

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const messageComponents = {
     secondPart: ['your friends', 'your colleagues', 'your family'],
     thirdPart: ['to do great things', 'to be there for others', 'to create something new']
 };
+
+function selectRandomComponent() {
+    selectFirstPart = messageComponents.firstPart[Math.floor(Math.random() * messageComponents.firstPart.length)];
+    selectSecondPart = messageComponents.secondPart[Math.floor(Math.random() * messageComponents.secondPart.length)];
+    selectThirdPart = messageComponents.thirdPart[Math.floor(Math.random() * messageComponents.thirdPart.length)];
+}
