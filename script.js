a587ce9355c1cda9db16a7ea0fe187b05ee4a821
Matthias Oppermann <mfoppermann@gmail.com>
2021-04-24 16:16:13 +0200

    call function and log message into the console

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const messageComponents = {
     thirdPart: ['to do great things', 'to be there for others', 'to create something new']
 };
 
-function selectRandomComponent() {
+function createRandomMessage() {
     selectFirstPart = messageComponents.firstPart[Math.floor(Math.random() * messageComponents.firstPart.length)];
     selectSecondPart = messageComponents.secondPart[Math.floor(Math.random() * messageComponents.secondPart.length)];
     selectThirdPart = messageComponents.thirdPart[Math.floor(Math.random() * messageComponents.thirdPart.length)];
+    console.log(`${selectFirstPart} ${selectSecondPart} ${selectThirdPart}.`);
 }
+
+createRandomMessage();
